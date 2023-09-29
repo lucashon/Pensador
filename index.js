7f70f5a7a619b13b5068f3f648d96b4a0dfb02e9
lucashon <lucas.honorio2916@gmail.com>
2023-09-29 14:08:51 -0300

    Criação das Pastas do Projeto e Conexão com o Banco

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+const app = express()
+const conn = require('./db/conn')
