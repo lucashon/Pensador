f01de5e5b4ea4a115a59735b0ce2cc347d5ca8e4
lucashon <lucas.honorio2916@gmail.com>
2023-10-11 14:13:23 -0300

    Auth e session

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -15,15 +15,51 @@ module.exports = class AuthController {
 
     if (password != confirmpassword) {
       request.flash("message", "As senhas não são iguais, tente novamente");
-      response.render("auth/register");
+      return response.render("auth/register");
     }
 
     // verificar se o email ja está cadastrado
+    const checkUserExist = await User.findOne({where: {email:email}})
+    if(checkUserExist){
+      request.flash("message", "O email já está em uso!");
+      response.render("auth/register");
+      return
+    }
 
     // Criptografar a senha do usuario
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt)
 
-    // criar o objeto do usuario para o banco
+    // criar o objeto do usuario para o banco:
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    }
 
     // try: inserir usuario no banco e trabalhara com sessão
+    try {
+
+    const createUser =  await User.create(user)
+
+     request.session.userId = createUser.id
+
+      request.flash('message', 'Cadastro realizado com sucesso!')
+
+      request.session.save(()=>{
+        response.redirect('/')
+      })
+
+
+    } catch (error) {
+      console.log(error)
+
+    }
   }
+
+   static async logout(request, response){
+    request.session.destroy()
+    response.redirect('/login')
+   }
+   
 };
--- routes/authRouters.js
+++ routes/authRouters.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { authPlugins } = require("mysql2");
 const router = express.Router();
 
 // Controller
@@ -7,5 +8,6 @@ const AuthController = require("../controllers/AuthController");
 router.get("/login", AuthController.login);
 router.get("/register", AuthController.register);
 router.post("/register", AuthController.registerPost);
+router.get('/logout', AuthController.logout)
 
 module.exports = router;
